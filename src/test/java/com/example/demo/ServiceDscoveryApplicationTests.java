71a25ac1150a3e110f3c4f70f52f9fb72cf10478
DESKTOP-UEKRUL2\DELL <paletivinayvarma@gmai.com>
2024-03-18 15:09:51 +0530

    initial commit

---
+++ src/test/java/com/example/demo/ServiceDscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
